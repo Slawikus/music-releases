683b9771d189b02b6e55edd2e41439e06943bf52
nikolay-igorev <43236160+nikolay-igorev@users.noreply.github.com>
2021-06-30 18:10:41 +0700

    feat: adding and removing profile currencies (#6)
    * feat: adding and removing profile currencies
    
    * fix: names of files and classes, other minor fixes
    
    * fix: minor issues
    
    * fix: url name

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import SignUpView, EditProfileView
+from .views import SignUpView, EditProfileView, CreateCurrencyView, DeleteCurrencyView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('profile/edit/', EditProfileView.as_view(), name='edit_profile'),
+    path('profile/edit/', EditProfileView.as_view(), name='profile_edit'),
+    path('currencies/new/', CreateCurrencyView.as_view(), name='currencies_create'),
+    path('currencies/<int:pk>/delete/', DeleteCurrencyView.as_view(), name='currency_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db import IntegrityError
 
 from .forms import CustomUserCreationForm, EditProfileForm
-from .models import Profile, User
+from .models import Profile, ProfileCurrency
 
 
 # Create your views here.
@@ -15,8 +16,36 @@ class SignUpView(CreateView):
 class EditProfileView(UpdateView):
     model = Profile
     form_class = EditProfileForm
-    template_name = 'edit_profile.html'
-    success_url = reverse_lazy('edit_profile')
+    template_name = 'profile_edit.html'
+    success_url = reverse_lazy('profile_edit')
 
     def get_object(self, queryset=None):
         return self.request.user.profile
+
+
+class CreateCurrencyView(CreateView):
+    model = ProfileCurrency
+    fields = ['currency']
+    template_name = 'currencies_list.html'
+    success_url = reverse_lazy('currencies_create')
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+    def form_valid(self, form):
+        form.instance.profile = self.request.user.profile
+        try:
+            return super().form_valid(form)
+        except IntegrityError:
+            form.add_error('currency', 'You have already chosen that currency')
+            return self.form_invalid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CreateCurrencyView, self).get_context_data(**kwargs)
+        context['currencies_list'] = ProfileCurrency.objects.filter(profile=self.request.user.profile)
+        return context
+
+
+class DeleteCurrencyView(DeleteView):
+    model = ProfileCurrency
+    success_url = reverse_lazy('currencies_create')
