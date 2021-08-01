2b0bf1e18f670bafd6d9db06f66f8b083d089dae
nikolay-igorev <43236160+nikolay-igorev@users.noreply.github.com>
2021-08-01 07:58:03 +0700

    Releases (#11)
    * feat: create release
    
    * fix: minor issues
    
    * feat: submit the release
    
    * delete DetailReleaseView
    
    * fix: minor issues
    
    * Delete python

--- releases/urls.py
+++ releases/urls.py
@@ -5,9 +5,9 @@ from .views import CreateReleaseView, AllReleaseView, RecentlyAddedView, Upcomin
 
 urlpatterns = [
     path('new/', CreateReleaseView.as_view(), name='release_add'),
+    path('<int:pk>/submit/', SubmitReleaseView.as_view(), name='release_submit'),
     path('all/', AllReleaseView.as_view(), name='all_releases'),
     path("upcoming/", UpcomingReleasesView.as_view(), name="upcoming"),
     path("recently-released/", RecentlyReleasedView.as_view(), name='recently_released'),
     path("recently-added/", RecentlyAddedView.as_view(), name="recently_added"),
     path("publish/<int:pk>", publish_release, name='publish')
-]
