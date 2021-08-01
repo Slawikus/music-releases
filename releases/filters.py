6db2a1363675ec28261d1107c93a798866dc2335
mrAdiletDev <adiletdj19@gmail.com>
2021-08-01 18:36:08 +0600

    filter fixed

--- releases/filters.py
+++ releases/filters.py
@@ -3,20 +3,7 @@ from .models import Release
 
 class ReleaseFilter(django_filters.FilterSet):
 
-    FORMATS = (
-        ("CD", 'CD'),
-        ("Vinyl", "Vinyl"),
-        ("Tape", "Tape"),
-        ("DVD", "DVD")
-    )
-
-    format = django_filters.ChoiceFilter(choices=FORMATS, method='filter_by_format')
-
-    def filter_by_format(self, queryset, name, value):
-
-        return queryset.filter(format=value)
-
     class Meta:
 
         model = Release
-        fields = ["base_style", "submitted_at"]
+        fields = ["format" ,"base_style", "submitted_at"]
