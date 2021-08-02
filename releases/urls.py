ff80c91c8ad645d714c87a381166e0ba29a76ad7
mrAdiletDev <adiletdj19@gmail.com>
2021-08-02 18:05:48 +0600

    testing excel

--- releases/urls.py
+++ releases/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import url
 from .views import (CreateReleaseView, AllReleaseView, MyReleasesView, 
                     UpcomingReleasesView, RecentlySubmittedView, EditReleaseView)
 
+from .excel import get_excel_file
+
 
 urlpatterns = [
     path('new/', CreateReleaseView.as_view(), name='release_add'),
@@ -11,5 +13,6 @@ urlpatterns = [
     url("upcoming/", UpcomingReleasesView.as_view(), name="upcoming_releases"),
     path("recently-submitted/", RecentlySubmittedView.as_view(), name='recently_submitted'),    
     path("my-releases/", MyReleasesView.as_view(), name='my_releases'),
-    path("edit-release/<int:pk>", EditReleaseView.as_view(), name='edit_release')
+    path("edit-release/<int:pk>", EditReleaseView.as_view(), name='edit_release'),
+    path("excel/", get_excel_file, name="excel")
 ]
