8acb70f479398084a9db46ebe61ff4ae0fabc82c
mrAdiletDev <adiletdj19@gmail.com>
2021-08-19 16:25:09 +0600

    Add: Autocomplete new version

--- releases/complete.py
+++ releases/complete.py
@@ -1,13 +1,15 @@
 from .models import Release
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseForbidden
 
 
 def get_band_name(request, query):
-    result = []
-    if query:
-        releases = Release.objects.filter(band_name__istartswith=query)
 
-        for release in releases:
-            result.append(release.band_name)
+    if not request.user.is_authenticated():
+        return HttpResponseForbidden
+
+    result = []
+    releases = Release.objects.filter(band_name__istartswith=query)
+    for release in releases:
+        result.append(release.band_name)
 
     return JsonResponse(result, safe=False)
