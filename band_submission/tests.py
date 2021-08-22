91c8438a3bed4f5290c687cf7f8c6b4e6bac8b75
mrAdiletDev <adiletdj19@gmail.com>
2021-08-22 18:30:51 +0600

    Add: test

--- band_submission/tests.py
+++ band_submission/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from users.models import Label
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 
 # Create your tests here.
+class BandSubmissionLinkTest(TestCase):
+    def setUp(self):
+        self.email = "test@gmail.com"
+        self.password = "test123456"
+        self.user = get_user_model().objects.create(self.email, self.password)
+
+    def test_it_creates_submission_link(self):
+        client = Client()
+        client.force_login(self.user)
+        response = client.post(reverse("submission_links"),)
