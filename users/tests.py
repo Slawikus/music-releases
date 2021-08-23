2aec1683b5d68e0c15bb8f6c8ca9a60c3344c151
mrAdiletDev <adiletdj19@gmail.com>
2021-08-23 15:14:41 +0600

    tests fixed

--- users/tests.py
+++ users/tests.py
@@ -8,17 +8,9 @@ class SignupPageTests(TestCase):
         self.email = 'newuser@email.com'
         self.password = 'TestPassword1234'
 
-    def test_signup_page_status_code(self):
-        response = self.client.get('/users/signup/')
-        self.assertEqual(response.status_code, 200)
-
     def test_view_url_by_name(self):
-        response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('signup'))
-        self.assertTemplateUsed(response, 'signup.html')
+        response = self.client.get(reverse('signup', args=['wrong_slug']))
+        self.assertEqual(response.status_code, 302)
 
     def test_signup_form(self):
         new_user = get_user_model().objects.create_user(self.email, self.password)
