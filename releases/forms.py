373dd9372684418982657b2c6b0a74d78cdc58ef
nikolay-igorev <43236160+nikolay-igorev@users.noreply.github.com>
2021-08-24 18:43:00 +0700

    Releases (#20)
    * feat: create release
    
    * fix: minor issues
    
    * feat: submit the release
    
    * delete DetailReleaseView
    
    * fix: minor issues
    
    * fix: conflicts
    
    * feat: Trades and Wholesale, Marketing Infos
    
    * fix: conflicts
    
    * feat: Trades and Wholesale, Wholesale prices
    
    * fix: minor issues
    
    * fix: minor issues
    
    * fix: getting the release object

--- releases/forms.py
+++ releases/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.forms import ModelForm
 
-from .models import Release, Label
+from configuration.settings import CURRENCY_CHOICES
+from users.models import ProfileCurrency
+from .models import Release, Label, WholesaleAndTrades, ReleaseWholesalePrice
 
 
 class DateInput(forms.DateInput):
@@ -11,7 +13,7 @@ class DateInput(forms.DateInput):
 class CreateReleaseForm(ModelForm):
     class Meta:
         model = Release
-        exclude = ['profile', 'is_submitted']
+        exclude = ['profile', 'is_submitted', 'submitted_at']
         widgets = {
             'release_date': DateInput(),
             'format': forms.RadioSelect,
@@ -27,6 +29,43 @@ class CreateReleaseForm(ModelForm):
 
         if self.instance:
             self.fields['label'].queryset = Label.objects.filter(profile=self.profile)
+
+
+class UpdateReleaseForm(ModelForm):
+    class Meta:
+        model = Release
+        fields = ['band_name', 'album_title', 'cover_image', 'sample', 'limited_edition']
+        widgets = {
+            'sample': forms.FileInput(attrs={'accept': 'application/mp3'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['cover_image'].widget.attrs.update({'class': 'form-control'})
+        self.fields['sample'].widget.attrs.update({'class': 'form-control'})
+
+
+class UpdateTradesAndWholesaleForm(ModelForm):
+    class Meta:
+        model = WholesaleAndTrades
+        exclude = ['release']
+        widgets = {
+            'available_for_trade': forms.RadioSelect,
+            'available_for_wholesale': forms.RadioSelect,
+        }
+
+
+class CreateWholesalePriceForm(ModelForm):
+    class Meta:
+        model = ReleaseWholesalePrice
+        exclude = ['release']
+
+    def __init__(self, release, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance:
+            self.fields['currency'].queryset = release.currencies_without_price()
+
 
 class ImportReleaseForm(forms.Form):
 
--- releases/urls.py
+++ releases/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
 from django.conf.urls import url
 
-from .views import (CreateReleaseView, AllReleaseView, MyReleasesView, ImportReleasesView, submit_release,
-                    UpcomingReleasesView, RecentlySubmittedView, EditReleaseView)
+from .views import (CreateReleaseView, AllReleaseView, MyReleasesView,
+                    UpcomingReleasesView, RecentlySubmittedView, EditReleaseView,
+                    UpdateWholesaleAndTradesView, CreateWholesalePriceView, SubmitReleaseView, ImportReleasesView)
 
 
 urlpatterns = [
     path('new/', CreateReleaseView.as_view(), name='release_add'),
+    path('<int:pk>/submit/', SubmitReleaseView.as_view(), name='release_submit'),
     path('', AllReleaseView.as_view(), name='all_releases'),
     url("upcoming/", UpcomingReleasesView.as_view(), name="upcoming_releases"),
-    path("recently-submitted/", RecentlySubmittedView.as_view(), name='recently_submitted'),    
+    path("recently-submitted/", RecentlySubmittedView.as_view(), name='recently_submitted'),
     path("my-releases/", MyReleasesView.as_view(), name='my_releases'),
-    path("<int:pk>/submit/", submit_release, name='submit_release'),
     path("<int:pk>/edit/", EditReleaseView.as_view(), name='edit_release'),
+    path('<int:pk>/wholesale_and_trades', UpdateWholesaleAndTradesView.as_view(), name='wholesale_and_trades_edit'),
+    path('<int:pk>/release_wholesale_price', CreateWholesalePriceView.as_view(), name='release_wholesale_price_add'),
     path("import-releases/", ImportReleasesView.as_view(), name="import_releases"),
 ]
