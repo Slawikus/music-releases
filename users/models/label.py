b4211baa08dbb09b4a9260c4d5645970be0a0dd7
mrAdiletDev <adiletdj19@gmail.com>
2021-09-07 11:02:17 +0600

    Users app file refactor

---
+++ users/models/label.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+# Create your models here.
+class Label(models.Model):
+    profile = models.ForeignKey(
+        "Profile",
+        on_delete=models.CASCADE,
+        related_name='label'
+    )
+    name = models.CharField(
+        max_length=250,
+        verbose_name='Label name',
+        help_text='Label name as you write on your releases',
+    )
+    logo = models.ImageField(
+        upload_to='images/labels/',
+        blank=True,
+        null=True,
+        verbose_name='Label logo',
+        help_text='For best result - black logo on white or transparent background',
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Short label description',
+    )
+    def __str__(self):
+        return self.name
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'profile'],
+                name='unique_label_per_profile'
+            ),
+        ]
---
+++ users/views/sign_up_view.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.views.generic import CreateView
+
+from users.forms import CustomUserCreationForm
+from users.models import Invitation
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = '/'
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            invitation = Invitation.objects.get(public_id=self.kwargs['public_id'])
+        except ObjectDoesNotExist:
+            return HttpResponse(self.request, "Sorry, your invitation link is not valid", status=200)
+        if not invitation.is_active:
+            return HttpResponse(self.request,
+                                "Sorry, your invitation link is already been used and not valid anymore")
+        return super(SignUpView, self).dispatch(request, *args, **kwargs)
+    def form_valid(self, form):
+        valid = super(SignUpView, self).form_valid(form)
+        if valid:
+            invitation = Invitation.objects.get(public_id=self.kwargs['public_id'])
+            invitation.is_active = False
+            invitation.save()
+        return valid
