7881b1a32f67c83b4aa36e9d053bc40df63bfeb6
mrAdiletDev <adiletdj19@gmail.com>
2021-09-07 11:14:13 +0600

    added default user

--- releases/management/commands/filldb.py
+++ releases/management/commands/filldb.py
@@ -1,7 +1,17 @@
 from django.core.management.base import BaseCommand
 from releases.factories import ReleaseFactory
+from users.models import User
+
+
+DEFAULT_EMAIL = 'admin@gmail.com'
+DEFAULT_PASSWORD = 'admin'
 
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		ReleaseFactory.create_batch(5, is_submitted=True)
+		user = User.objects.create(email=DEFAULT_EMAIL,
+								   password=DEFAULT_PASSWORD,
+								   is_superuser=True
+								   )
+
+		ReleaseFactory.create_batch(5, is_submitted=True, profile=user.profile)
