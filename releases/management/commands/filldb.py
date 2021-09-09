b39e6c707651f7560c9fb854bee976562a7622c6
mrAdiletDev <adiletdj19@gmail.com>
2021-09-09 13:36:21 +0600

    changed to superuser

--- releases/management/commands/filldb.py
+++ releases/management/commands/filldb.py
@@ -9,10 +9,11 @@ DEFAULT_PASSWORD = 'admin'
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		user = User.objects.create(email=DEFAULT_EMAIL,
-								   password=DEFAULT_PASSWORD,
-								   is_superuser=True
-								   )
+		user = User.objects.create(
+			email=DEFAULT_EMAIL,
+			password=DEFAULT_PASSWORD,
+			is_superuser=True
+		)
 
 		ReleaseFactory.create_batch(3, is_submitted=True, profile=user.profile)
 		ReleaseFactory.create_batch(2, profile=user.profile)
