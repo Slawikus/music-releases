dea626fe6c666a6d037e8b2c12be24c9f211099c
Nikolay Igorev <frololo.frololo@yandex.ru>
2021-09-11 18:34:41 +0700

    fix: minor issues

--- releases/models.py
+++ releases/models.py
@@ -118,24 +118,6 @@ class Release(models.Model):
         return currency_choices
 
 
-# class WholesaleAndTrades(models.Model):
-#     YES_NO_CHOICES = (
-#         (True, 'Yes'),
-#         (False, 'No')
-#     )
-#
-#     release = models.OneToOneField(Release, on_delete=models.CASCADE)
-#     available_for_trade = models.BooleanField(default=False, choices=YES_NO_CHOICES)
-#     trade_points = models.DecimalField(
-#         decimal_places=1,
-#         max_digits=3,
-#         validators=[MinValueValidator(0), MaxValueValidator(30)],
-#         null=True,
-#         blank=True
-#     )
-#     trade_remarks = models.CharField(max_length=250, null=True, blank=True)
-#     available_for_wholesale = models.BooleanField(default=False, choices=YES_NO_CHOICES)
-
 YES_NO_CHOICES = (
         (True, 'Yes'),
         (False, 'No')
