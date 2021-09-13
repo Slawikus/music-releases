62a8806de234b496513c1ead46545a0617eadfeb
mrAdiletDev <adiletdj19@gmail.com>
2021-09-13 14:27:09 +0600

    regex parse version

--- artists/management/commands/parsenames.py
+++ artists/management/commands/parsenames.py
@@ -1,7 +1,6 @@
 from django.core.management import BaseCommand
-from lxml import etree
 from artists.models import Artist
-
+import re
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
@@ -10,19 +9,10 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 
 		file = open(options["path"], "r")
-		parser = etree.XMLParser(recover=True)
-
-		artist = ""
 		while True:
-			string = file.read(1)
+			string = file.read(1024 * 10)
 			if string is None:
 				break
-			artist += string
-			if "</artist>" in artist and "<artist>" in artist:
-				tree = etree.fromstring(artist, parser=parser)
-				artist = ""
-				if tree is None:
-					print("parse failed")
-				else:
-					for name in tree.findall(".//name"):
-						Artist.objects.get_or_create(name=name.text)
+			res = re.findall(r"<name>[a-zA-Z0-9 ]+</name>", string)
+			for name in res:
+				Artist.objects.get_or_create(name=name[6:-7])
