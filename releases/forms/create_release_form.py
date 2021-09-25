c5b1dbca73eb5f105a4c3e35b2cda6349604a2fc
mrAdiletDev <adiletdj19@gmail.com>
2021-09-25 18:37:25 +0600

    Fix: oops

--- releases/forms/create_release_form.py
+++ releases/forms/create_release_form.py
@@ -35,9 +35,6 @@ class CreateReleaseForm(ModelForm):
     def clean_cover_image(self):
         cover_image = self.cleaned_data['cover_image']
 
-        if not cover_image:
-            with open(f"{BASE_DIR}/releases/test_files/dummy.jpg", 'rb') as dummy_jpg:
-                cover_image = dummy_jpg
 
         width, height = get_image_dimensions(cover_image)
 
