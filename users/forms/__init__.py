bae74f776ff88ed3136c38cfdc1df2b2e6bb5abe
mrAdiletDev <adiletdj19@gmail.com>
2021-09-25 18:49:59 +0600

    Delete: unused form Refactor: add whitespace at the end of files

--- users/forms/__init__.py
+++ users/forms/__init__.py
@@ -1,4 +1,3 @@
-from .combined_form_base import CombinedFormBase
 from .create_currency_form import CreateCurrencyForm
 from .custom_user_change_form import CustomUserChangeForm
 from .custom_user_creation_form import CustomUserCreationForm
@@ -8,7 +7,6 @@ from .user_profile_creation_form import UserProfileCreationForm
 
 
 __all__ = [
-	CombinedFormBase,
 	CreateCurrencyForm,
 	CustomUserChangeForm,
 	CustomUserCreationForm,
