1a047ab4604019d883a70ace21a5a09a69a39eee
mrAdiletDev <adiletdj19@gmail.com>
2021-09-25 20:52:30 +0600

    Add: tests

--- releases/tests/create_release_test.py
+++ releases/tests/create_release_test.py
@@ -12,7 +12,7 @@ class CreateReleaseTest(BaseClientTest):
 
         self.assertEqual(response.status_code, 200)
 
-    def test_it_creates_the_release(self):
+    def test_it_creates_and_submits_the_release(self):
         label = LabelFactory(profile=self.user.profile)
 
         album_title = 'Some Album Title'
@@ -38,3 +38,29 @@ class CreateReleaseTest(BaseClientTest):
         self.assertEqual(Release.objects.count(), 1)
         self.assertEqual(Release.objects.first().album_title, album_title)
         self.assertRedirects(response, reverse("my_releases"))
+
+        release = Release.objects.get(album_title=album_title)
+        self.client.post(reverse('release_submit', args=[release.id]))
+        release.refresh_from_db()
+        self.assertTrue(release.is_submitted)
+
+    def test_it_creates_not_full_release(self):
+        band_name = 'test_band'
+        response = self.client.post(reverse_lazy('release_add'), {
+            'band_name': band_name,
+            'album_title': 'test_title'
+        })
+
+        self.assertTrue(Release.objects.filter(band_name=band_name).exists())
+        self.assertRedirects(response, expected_url=reverse_lazy('my_releases'))
+
+    def test_it_forbids_submit_not_full_release(self):
+        release = Release.objects.create(
+            profile=self.user.profile,
+            band_name='test_band',
+            album_title='test_title'
+        )
+
+        self.client.post(reverse('release_submit', args=[release.id]))
+        release.refresh_from_db()
+        self.assertFalse(release.is_submitted)
--- releases/views/submit_release_view.py
+++ releases/views/submit_release_view.py
@@ -13,17 +13,26 @@ class SubmitReleaseView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     login_url = 'login'
     success_url = reverse_lazy('all_releases')
 
-    def form_valid(self, form):
-        form.instance.is_submitted = True
-        form.instance.submitted_at = timezone.datetime.now()
-        return super().form_valid(form)
-
     def test_func(self):
         obj = self.get_object()
         return obj.profile == self.request.user.profile
 
     def post(self, request, *args, **kwargs):
         release = Release.objects.get(pk=kwargs['pk'])
-        if None in list(release.__dict__.values()):
+        _fields_dict = release.__dict__
+        del _fields_dict['submitted_at']
+
+        fields = list(_fields_dict.values())
+
+        if None in fields:
             messages.error(request, "all fields must filled")
+            return HttpResponseRedirect(reverse_lazy('my_releases'))
+
+        else:
+
+            release.is_submitted = True
+            release.submitted_at = timezone.datetime.now()
+            release.save()
+
+            messages.success(request, "successfully submitted")
             return HttpResponseRedirect(reverse_lazy('my_releases'))
