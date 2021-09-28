ebf5f439b3e21d6c0498e3f6234f1383b9e2a551
mrAdiletDev <adiletdj19@gmail.com>
2021-09-28 16:48:53 +0600

    Ready

--- releases/views/base_release.py
+++ releases/views/base_release.py
@@ -5,7 +5,7 @@ from releases.models import Release
 
 class BaseRelease(LoginRequiredMixin, ListView):
     context_object_name = "releases"
-    paginate_by = 2
+    paginate_by = 21
     template_name = "release/release_list.html"
     model = Release
 
