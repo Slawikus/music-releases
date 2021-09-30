88b0cc9208a26412479081313fe4e621bc465fca
mrAdiletDev <adiletdj19@gmail.com>
2021-09-30 19:43:03 +0600

    Formatting

--- releases/models/favourite_release.py
+++ releases/models/favourite_release.py
@@ -4,11 +4,5 @@ from releases.models import Release
 
 
 class FavouriteRelease(models.Model):
-	profile = models.ForeignKey(Profile,
-								related_name='favourite_releases',
-								on_delete=models.CASCADE
-								)
-	release = models.OneToOneField(Release,
-								   related_name='favourite_object',
-								   on_delete=models.CASCADE
-								)
+	profile = models.ForeignKey(Profile, related_name='favourite_releases', on_delete=models.CASCADE)
+	release = models.OneToOneField(Release, related_name='favourite_object', on_delete=models.CASCADE)
