d281c3e600957c2ed14878a91acaa617603e8de8
mrAdiletDev <adiletdj19@gmail.com>
2021-09-30 19:41:23 +0600

    Ready

--- releases/views/base_release.py
+++ releases/views/base_release.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from releases.models import Release, FavouriteRelease
 
-from releases.models import Release
 
 class BaseRelease(LoginRequiredMixin, ListView):
     context_object_name = "releases"
@@ -9,3 +9,8 @@ class BaseRelease(LoginRequiredMixin, ListView):
     template_name = "release/release_list.html"
     model = Release
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        pks = list(FavouriteRelease.objects.values_list('release', flat=True))
+        context['fav_releases'] = pks
+        return context
