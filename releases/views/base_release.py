293ccd91cc827b708b0dff4918f960507e96056c
mrAdiletDev <adiletdj19@gmail.com>
2021-10-01 17:29:48 +0600

    wishlist ready

--- releases/views/base_release.py
+++ releases/views/base_release.py
@@ -7,3 +7,8 @@ class BaseRelease(LoginRequiredMixin, ListView):
     context_object_name = "releases"
     template_name = "release/release_list.html"
     model = Release
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wished_ids'] = list(self.request.user.profile.wishlist.values_list("id", flat=True))
+        return context
