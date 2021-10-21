76eeee7ee802666f368d8b3d78767bb0467d1c4f
mrAdiletDev <adiletdj19@gmail.com>
2021-10-21 20:11:42 +0600

    temp

--- public_tradelist/models/trade_request_items.py
+++ public_tradelist/models/trade_request_items.py
@@ -21,10 +21,31 @@ def create_trade_request(form, trade_item_model, profile):
         release = Release.objects.get(id=release_id)
         trade_item_model.objects.create(trade_request=trade_request,
                                         release=release,
-                                        quantity=quantity)
+                                        quantity=quantity,
+                                        band_name=release.band_name,
+                                        release_date=release.release_date,
+                                        trade_points=release.releasetradeinfo.trade_points,
+                                        currency=release.releasewholesaleinfo.currency.currency,
+                                        price=release
+                                        )
 
 
 class TradeRequestItem(models.Model):
     trade_request = models.ForeignKey(TradeRequest, on_delete=models.CASCADE, related_name="trade_items")
     release = models.ForeignKey(Release, on_delete=models.CASCADE, related_name="trade_items")
+    band_name = models.CharField(
+        max_length=250,
+        verbose_name='Band name(s)',
+    )
+    release_date = models.DateField(
+        verbose_name='Release date',
+    )
+    trade_points = models.DecimalField(
+        decimal_places=1,
+        max_digits=3,
+    )
+
+    currency = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+
     quantity = models.PositiveIntegerField()
