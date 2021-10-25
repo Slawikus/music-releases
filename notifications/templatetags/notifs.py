32df60f889461c19bcb84e3f55569586f7a53083
mrAdiletDev <adiletdj19@gmail.com>
2021-10-25 17:37:34 +0600

    Problem: tags is not been called in template

--- notifications/templatetags/notifs.py
+++ notifications/templatetags/notifs.py
@@ -1,13 +1,15 @@
 from django import template
-
+from notifications.models import Notification
 register = template.Library()
 
-@register.filter
-def notifs_amount(user):
-	return user.profile.notifications.filter(is_viewed=False).count()
+@register.simple_tag(takes_context=True)
+def number_of_unread_notifications(context):
+	user = context["user"]
+	return Notification.objects.unread_by(profile=user.profile).count()
 
 
-@register.filter
-def notifs_list(user):
-	notifications = user.profile.notifications.filter(is_viewed=False)
+@register.simple_tag(takes_context=True)
+def last_file_notifications(context):
+	user = context["user"]
+	notifications = Notification.objects.unread_by(profile=user.profile)
 	return notifications[:5]
