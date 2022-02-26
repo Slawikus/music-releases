1357964bb5042283e57f09756a700b3fd997c2d0
Nikolay Igorev <frololo.frololo@yandex.ru>
2022-02-26 18:30:39 +0700

    feat: vuexy profile edit form

--- users/forms/edit_profile_form.py
+++ users/forms/edit_profile_form.py
@@ -6,3 +6,7 @@ class EditProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['country', 'address']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['address'].widget.attrs['rows'] = 5
